fc417adf44a8b5a9f521ddc8eb99dd72b189961d
Khasbilegt.TS <9023312+khasbilegt@users.noreply.github.com>
2020-07-18 18:10:14 +0800

    close #8 (#12)
    Үндсэн фонтоор "Rubik" -г ашиглахаар болж, тохируулсан.

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: process.env.API_URL || "http://localhost:1337",
+        apiURL: process.env.API_URL,
         queryLimit: 1000, // Default to 100
         contentTypes: [`post`, `tag`],
       },
