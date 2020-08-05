a74f2f8dd40d48bf68b632043ef1999ee72a6a20
Bilguun Ochirbat <bilguun0203@gmail.com>
2020-08-05 18:25:07 +0800

    refactor to camel case

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Seo, { SeoProps } from "./seo";
+import Seo, { SeoProps } from "./Seo";
 
 import logo from "../assets/images/black.png";
-import style from "./layout.module.scss";
+import style from "./Layout.module.scss";
 
 interface LayoutProps {
   children: JSX.Element[] | JSX.Element;
