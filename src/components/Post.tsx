2f4491de529b1711f5bfa74eb18aa527ced0cccb
Bilguun Ochirbat <bilguun0203@gmail.com>
2020-09-07 23:34:20 +0800

    designing 2/x (post page & tweaks)

---
+++ src/components/Post.tsx
@@ -0,0 +1,109 @@
+import React from "react";
+import Layout from "../components/Layout";
+import { Link } from "gatsby";
+import Img, { FluidObject } from "gatsby-image";
+import { MDXProvider } from "@mdx-js/react";
+import { MDXRenderer } from "gatsby-plugin-mdx";
+import style from "./Post.module.scss";
+
+type Post = {
+  strapiId: number;
+  slug: string;
+  title: string;
+  excerpt: string;
+  publish_at: Date;
+  content: string;
+  status: string;
+  childMdx: {
+    body: string;
+    tableOfContents: {
+      items: [{ url: string; title: string }];
+    };
+    timeToRead: number;
+    wordCount: {
+      paragraphs: number;
+      sentences: number;
+      words: number;
+    };
+  };
+  author: {
+    id: number;
+    username: string;
+    email: string;
+    full_name: string;
+  };
+  tags: [{ id: number; name: string; slug: string }];
+  image: {
+    childImageSharp: {
+      fluid: FluidObject;
+    };
+  };
+};
+
+function TestComponent() {
+  return <code>Hello World!</code>;
+}
+
+function PostHeader({ post }: { post: Post }) {
+  return (
+    <div className={style.postHeader}>
+      <div className={style.postInfo}>
+        <p className={style.postTag}>
+          {post.tags.length > 0 ? (
+            <Link to={`/tag/${post.tags[0].slug}`}>{post.tags[0].name}</Link>
+          ) : null}
+        </p>
+        <h1 className={style.postTitle}>{post.title}</h1>
+        <p className={style.postExcerpt}>{post.excerpt}</p>
+        <hr />
+        <p className={style.postAuthor}>
+          {post.author ? (
+            <Link to={`/author/${post.author.username}`}>
+              {post.author.full_name
+                ? post.author.full_name
+                : post.author.username}
+            </Link>
+          ) : (
+            "Байхгүй"
+          )}
+        </p>
+        <p className={style.postDateAndTimeToRead}>
+          {post.publish_at} •{" "}
+          {post.childMdx.timeToRead ? post.childMdx.timeToRead : 0} мин
+        </p>
+      </div>
+      {post.image !== null ? (
+        <Img fluid={post.image.childImageSharp.fluid} />
+      ) : null}
+    </div>
+  );
+}
+
+export default function Post({ post }: { post: Post }) {
+  const toc = post.childMdx.tableOfContents;
+  return (
+    <Layout>
+      <main className="grid-container">
+        <div className="grid-x">
+          <div className="cell auto">
+            <PostHeader post={post} />
+            {Object.keys(toc).length > 0 ? (
+              <ul>
+                {toc.items.map(i => (
+                  <li key={i.url}>
+                    <a href={i.url} key={i.url}>
+                      {i.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            ) : null}
+            <MDXProvider components={{ TestComponent }}>
+              <MDXRenderer>{post.childMdx.body}</MDXRenderer>
+            </MDXProvider>
+          </div>
+        </div>
+      </main>
+    </Layout>
+  );
+}
--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-import Layout from "../components/Layout";
-import { graphql, Link } from "gatsby";
-import Img, { FluidObject } from "gatsby-image";
-import { MDXProvider } from "@mdx-js/react";
-import { MDXRenderer } from "gatsby-plugin-mdx";
+import PostComponent from "../components/Post";
+import { graphql } from "gatsby";
+import { FluidObject } from "gatsby-image";
 
 type Post = {
   strapiId: number;
   slug: string;
   title: string;
-  created_at: Date;
+  excerpt: string;
+  publish_at: Date;
   content: string;
   status: string;
   childMdx: {
@@ -30,6 +29,7 @@ type Post = {
     email: string;
     full_name: string;
   };
+  tags: [{ id: number; name: string; slug: string }];
   image: {
     childImageSharp: {
       fluid: FluidObject;
@@ -37,56 +37,8 @@ type Post = {
   };
 };
 
-function TestComponent() {
-  return <code>Hello World!</code>;
-}
-
 export default function Post({ data }: { data: { post: Post } }) {
-  const toc = data.post.childMdx.tableOfContents;
-  return (
-    <Layout>
-      <main className="grid-container">
-        <div className="grid-x">
-          <div className="cell auto">
-            <p>{data.post.created_at}</p>
-            <h1>{data.post.title}</h1>
-            {data.post.image !== null ? (
-              <Img fluid={data.post.image.childImageSharp.fluid} />
-            ) : null}
-            {Object.keys(toc).length > 0 ? (
-              <ul>
-                {toc.items.map(i => (
-                  <li key={i.url}>
-                    <a href={i.url} key={i.url}>
-                      {i.title}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            ) : null}
-            <MDXProvider components={{ TestComponent }}>
-              <MDXRenderer>{data.post.childMdx.body}</MDXRenderer>
-            </MDXProvider>
-            <p>Time to read: {data.post.childMdx.timeToRead}</p>
-            <p>Paragraphs: {data.post.childMdx.wordCount.paragraphs}</p>
-            <p>Sentences: {data.post.childMdx.wordCount.sentences}</p>
-            <p>Words: {data.post.childMdx.wordCount.words}</p>
-            <p>{data.post.status}</p>
-            <p>
-              Author:{" "}
-              {data.post.author !== null ? (
-                <Link to={`/author/${data.post.author.username}`}>
-                  {data.post.author.username}
-                </Link>
-              ) : (
-                <span>Null</span>
-              )}
-            </p>
-          </div>
-        </div>
-      </main>
-    </Layout>
-  );
+  return <PostComponent post={data.post} />;
 }
 
 export const query = graphql`
@@ -120,7 +72,12 @@ export const query = graphql`
         email
         full_name
       }
-      created_at(formatString: "YYYY-MM-DD")
+      tags {
+        id
+        name
+        slug
+      }
+      publish_at(formatString: "YYYY/MM/DD")
     }
   }
 `;
