08cb8c3ea8e9c6cfe8a7e82cace1e2b19281e449
Bilguun Ochirbat <bilguun0203@gmail.com>
2020-09-29 23:46:54 +0800

    prismjs material theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,6 @@
 require("prismjs/themes/prism-solarizedlight.css");
 require("prismjs/plugins/line-numbers/prism-line-numbers.css");
 require("prismjs/plugins/command-line/prism-command-line.css");
+require("prismjs-material-theme/css/oceanic.min.css");
 require("./src/styles/code-highlight.css");
 import "./src/styles/global.scss";
